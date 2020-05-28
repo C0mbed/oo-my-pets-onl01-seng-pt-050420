2fb79e6ce031f6424a1e2e82092a9416a822a6f8
Matt Farncombe <matt@farncombe.co>
2020-05-28 14:53:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,20 @@
 class Owner
   attr_reader :name, :species
 
+  @@all = []
+
   def initialize(name, species="human")
     @name = name
     @species = species
+    @@all << self
   end
 
   def say_species
     return "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
   end
 
 
