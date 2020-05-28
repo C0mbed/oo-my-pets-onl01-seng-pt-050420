28551560bfe6269f2caf79733505daef585d2c3d
Matt Farncombe <matt@farncombe.co>
2020-05-28 14:59:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
-  # code goes here
+  attr_accessor :owner ;mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner, mood="nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+  end
 end
