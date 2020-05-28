c2c8a0c382642d64c3985be93fa8616a08d9c2c5
Matt Farncombe <matt@farncombe.co>
2020-05-28 14:57:42 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,9 +2,16 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
 
+  @@all = []
+
   def initialize(name, owner, mood="nervous")
     @name = name
     @owner = owner
     @mood = mood
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
